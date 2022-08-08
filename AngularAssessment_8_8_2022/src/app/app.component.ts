7ffeefc2b18375013d2395930abea5bb14e27499
DibsGhosh <ghostdibyendu@gmail.com>
2022-08-08 11:11:13 +0530

    Assessment

---
+++ AngularAssessment_8_8_2022/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularAssessment_8_8_2022';
+}
